0ef8edfcb59d90e3aeadf70ecb4f6bb80ca682af
Ricardo Andres Casallas Ariza <ricardoandrescasallas@gmail.com>
2024-01-16 16:59:47 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Amoba_Medic_Doctor"
+include(":Amoba_Medic_Doctor")
+include(":shared")
+include(":app")
